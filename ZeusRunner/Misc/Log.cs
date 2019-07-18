4311e237558e7be180ce50c6ffe827787b358422
$rape001 <$rape001@C2W28528>
2019-07-18 10:23:11 -0500

    fixed credential errors when installing. Added example of how the credentials file should be made

---
+++ ZeusRunner/Misc/Log.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Text;
+using System;
+
+namespace Logs
+{
+    public class Log
+    {
+        public Log()
+        {   }
+
+        public void captureError(string msg)
+        {   saveLogFile("Error",msg);   }
+
+        public void captureEvent(string msg)
+        {   saveLogFile("Event", msg);  }
+
+        public static void saveLogFile(string filename,string msg)
+        {   String ruta = filename;
+            IFormatter formateador = new BinaryFormatter();
+            byte[] a;
+            char[] b;
+            b = msg.ToCharArray();
+            a = new byte[b.Length];
+            lock(a)
+            {	Stream flujo = new FileStream(ruta + ".log",FileMode.Append,FileAccess.Write);
+				for(int i = 0;i < a.Length;i++)
+            	{   a[i] = (byte)b[i]; }
+            	try
+                {   string t = "\r\n\r\n" + DateTime.Now.ToString();
+                    t = t + Environment.NewLine;
+                    byte[] tm = Encoding.ASCII.GetBytes(t);
+                    flujo.Write(tm,0,tm.Length);
+                    flujo.Write(a,0,a.Length);
+                }
+                catch(Exception){}
+                flujo.Close();
+            }
+        }
+    }
+}
